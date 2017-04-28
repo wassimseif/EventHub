4984ec402f1cbabb0327d2a33edeed0132ac1e5e
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2017-04-28 14:11:18 +0300

    Initial Commit

---
+++ EventHub/EventHub.h
@@ -0,0 +1,19 @@
+//
+//  EventHub.h
+//  EventHub
+//
+//  Created by Wassim on 4/28/17.
+//  Copyright © 2017 Wassim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventHub.
+FOUNDATION_EXPORT double EventHubVersionNumber;
+
+//! Project version string for EventHub.
+FOUNDATION_EXPORT const unsigned char EventHubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventHub/PublicHeader.h>
+
+
